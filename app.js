cb9e3d489bbe83038e9d4678688ad7f917877523
mrktegerero <kurtrendel@gmail.com>
2021-06-11 19:07:58 +0800

    new commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// SEE MORE //
+
+//var i=0;
+//function read(){
+  //  if(!i){
+    //    document.getElementsByClassName("tbody-leaderboard-seemore")[0].style.display="inline-block";
+      //  document.getElementById("read").innerHTML="See less";
+        //i=1;
+    //}
+    //else{
+      //  document.getElementsByClassName("tbody-leaderboard-seemore")[0].style.display="none";
+        //document.getElementsByClassName("tbody-leaderboard-seemore")[0].style.width="100%";
+        //document.getElementById("read").innerHTML="See more";
+        //i=0;
+    //}
+//}
